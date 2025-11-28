e9e4f305de28efdd00d4eba3f8e3efb26ce0f48f
royporat <roy.porat@bloxroute.com>
2025-11-28 20:41:02 +0200

    category

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import './Header.css';
 import Logo from '../ui/Logo/Logo';
 import SearchInput from '../SearchInput/SearchInput';
+import CategorySelector from '../CategorySelector/CategorySelector';
 
 const Header: React.FC = () => {
   return (
     <header className="header">
       <div className="header-container">
         <Logo size="M" />
-        <SearchInput />
+        <div className="header-content">
+          <SearchInput />
+          <CategorySelector />
+        </div>
       </div>
     </header>
   );
