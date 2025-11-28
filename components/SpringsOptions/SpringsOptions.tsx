e7d3bb19a594009a520e0df3655a2e7c82924db2
royporat <roy.porat@bloxroute.com>
2025-11-28 20:41:02 +0200

    category

--- components/SpringsOptions/SpringsOptions.tsx
+++ components/SpringsOptions/SpringsOptions.tsx
@@ -4,32 +4,36 @@ import React, { useEffect, useState } from 'react';
 import Grid from '../ui/Grid/Grid';
 import Card from '../ui/Card/Card';
 import './SpringsOptions.css';
-import {Spring} from "@/models/types/spring"
-import listSprings from "@/app/actions/listSprings";
-
+import { Spring } from '@/models/types/spring';
+import listSprings from '@/app/actions/listSprings';
 
 const SpringsOptions: React.FC = () => {
   const [springs, setSprings] = useState<Spring[]>([]);
 
-    useEffect(() => {
-        const fetchSprings = async () => {
-            const springResponse = await listSprings();
-            if (!springResponse || springResponse.data?.length === 0) {
-                return;
-            }
+  useEffect(() => {
+    const fetchSprings = async () => {
+      const springResponse = await listSprings();
+      if (!springResponse || springResponse.data?.length === 0) {
+        return;
+      }
 
-            setSprings(springResponse.data || [])
-        }
+      setSprings(springResponse.data || []);
+    };
 
-        fetchSprings();
-    }, []);
+    fetchSprings();
+  }, []);
 
   return (
     <div className="springs-options-container">
       <h2 className="springs-options-title">Explore Springs ({springs.length})</h2>
       <Grid>
         {springs.map((spring) => (
-          <Card id={spring._id} key={spring._id} title={spring.name} description={spring.fullDescription} />
+          <Card
+            id={spring._id}
+            key={spring._id}
+            title={spring.name}
+            description={spring.fullDescription}
+          />
         ))}
       </Grid>
     </div>
