dfa3f71c02120e2f66945c4f9a8df30b35f21781
eladlaorblinkops <elad.laor@blinkops.com>
2025-11-28 20:39:31 +0200

    springs markers

--- components/SpringsOptions/SpringsOptions.tsx
+++ components/SpringsOptions/SpringsOptions.tsx
@@ -3,33 +3,20 @@
 import React, { useEffect, useState } from 'react';
 import Grid from '../ui/Grid/Grid';
 import Card from '../ui/Card/Card';
-import listSprings from "@/app/actions/listSprings";
-import './SpringsOptions.css';
-import {Spring} from "@/models/types/spring"
+import { Spring } from '@/models/types/spring';
 
-
-const SpringsOptions: React.FC = () => {
-  const [springs, setSprings] = useState<Spring[]>([]);
-
-    useEffect(() => {
-        const fetchSprings = async () => {
-            const springResponse = await listSprings();
-            if (!springResponse || springResponse.data?.length === 0) {
-                return;
-            }
-
-            setSprings(springResponse.data || [])
-        }
-
-        fetchSprings();
-    }, []);
-
+const SpringsOptions: React.FC<{ springs: Spring[] }> = ({ springs }) => {
   return (
     <div className="springs-options-container">
       <h2 className="springs-options-title">Explore Springs ({springs.length})</h2>
       <Grid>
         {springs.map((spring) => (
-          <Card id={spring._id} key={spring._id} title={spring.name} description={spring.fullDescription} />
+          <Card
+            id={spring._id}
+            key={spring._id}
+            title={spring.name}
+            description={spring.fullDescription}
+          />
         ))}
       </Grid>
     </div>
