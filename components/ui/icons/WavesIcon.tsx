e3515132c2476919f9eae1f6d9c5f8d484846122
royporat <roy.porat@bloxroute.com>
2025-11-29 17:33:31 +0200

    tags list

---
+++ components/ui/icons/WavesIcon.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+type WavesIconProps = {
+  width?: number | string;
+  height?: number | string;
+  className?: string;
+  strokeWidth?: number;
+};
+
+const WavesIcon: React.FC<WavesIconProps> = ({
+  width = 24,
+  height = 24,
+  className,
+  strokeWidth = 1.5,
+}) => {
+  return (
+    <svg
+      width={width}
+      height={height}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+      strokeWidth={strokeWidth}
+    >
+      <path
+        d="M3 10C5.48276 10 7.34483 7 7.34483 7C7.34483 7 9.2069 10 11.6897 10C14.1724 10 16.6552 7 16.6552 7C16.6552 7 19.1379 10 21 10"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M3 17C5.48276 17 7.34483 14 7.34483 14C7.34483 14 9.2069 17 11.6897 17C14.1724 17 16.6552 14 16.6552 14C16.6552 14 19.1379 17 21 17"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default WavesIcon;
