bc48ec937c89c77a550f014d3071c3e70180e0a7
royporat <roy.porat@bloxroute.com>
2025-11-29 13:32:39 +0200

    news box

---
+++ components/NearbySpring/NearbySpring.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NearbySpring.css';
+
+const NearbySpring = () => {
+  return (
+    <div>
+      <h2>Nearby Springs</h2>
+    </div>
+  );
+};
+
+export default NearbySpring;
