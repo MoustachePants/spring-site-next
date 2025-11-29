82c393ac1229bbc8690d00b34b592cf580513a09
royporat <roy.porat@bloxroute.com>
2025-11-29 08:54:37 +0200

    refactor

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,72 @@
-// import { redirect } from 'next/navigation';
-// import router from '../routes';
+'use client';
 
-export default function Home() {
-  // Server-side redirect to login page
-  // redirect(router.dashboard.p);
-}
+import { useEffect, useState } from 'react';
+import { NextPage } from 'next';
+import dynamic from 'next/dynamic';
+import './home.css';
+import { Spring } from '@/models/types/spring';
+import { UserLocation } from '@/models/types/userLocation';
+import listSprings from '@/app/actions/listSprings';
+import getSpring from '@/app/actions/getSpring';
+import DetailsPanel from '@/components/DetailsPanel/DetailsPanel';
+
+const Map = dynamic(() => import('@/components/Map/Map'), { ssr: false });
+
+const Home: NextPage = () => {
+  const [springs, setSprings] = useState<Spring[]>([]);
+  const [selectedSpring, setSelectedSpring] = useState<Spring>();
+  const [userLocation, setUserLocation] = useState<UserLocation | null>(null);
+
+  useEffect(() => {
+    const fetchSpring = async () => {
+      if (selectedSpring?._id) {
+        const spring = await getSpring(selectedSpring?._id);
+        console.log('spring:', spring);
+      }
+    };
+
+    fetchSpring();
+  }, [selectedSpring]);
+
+  useEffect(() => {
+    const fetchSprings = async () => {
+      const springResponse = await listSprings();
+      if (!springResponse || springResponse.data?.length === 0) {
+        return;
+      }
+
+      console.log(springResponse.data);
+      setSprings(springResponse.data || []);
+    };
+
+    fetchSprings();
+  }, []);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined' && 'geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setUserLocation({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          });
+        },
+        (error) => {
+          console.error('Error getting user location:', error);
+          // Optionally handle error (e.g., show a toast notification)
+        }
+      );
+    }
+  }, []);
+
+  return (
+    <main className="dashboard-container">
+      <DetailsPanel springs={springs} setSelectedSpring={setSelectedSpring} />
+      <section className="map-wrapper">
+        <Map springs={springs} selectedSpring={selectedSpring} userLocation={userLocation} />
+      </section>
+    </main>
+  );
+};
+
+export default Home;
