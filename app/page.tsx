1c4a56934ee6e0ded57f55d2cfcbbee1b49bd6f5
royporat <roy.porat@bloxroute.com>
2025-11-29 10:41:43 +0200

    refactor

--- app/page.tsx
+++ app/page.tsx
@@ -1,69 +1,25 @@
 'use client';
 
-import { useEffect, useState } from 'react';
 import { NextPage } from 'next';
 import dynamic from 'next/dynamic';
 import './home.css';
-import { Spring } from '@/models/types/spring';
-import { UserLocation } from '@/models/types/userLocation';
-import listSprings from '@/app/actions/listSprings';
-import getSpring from '@/app/actions/getSpring';
 import DetailsPanel from '@/components/DetailsPanel/DetailsPanel';
+import { useDataContext } from '@/context/DataContext';
 
 const Map = dynamic(() => import('@/components/Map/Map'), { ssr: false });
 
 const Home: NextPage = () => {
-  const [springs, setSprings] = useState<Spring[]>([]);
-  const [selectedSpring, setSelectedSpring] = useState<Spring>();
-  const [userLocation, setUserLocation] = useState<UserLocation | null>(null);
-
-  useEffect(() => {
-    const fetchSpring = async () => {
-      if (selectedSpring?._id) {
-        const spring = await getSpring(selectedSpring?._id);
-        console.log('spring:', spring);
-      }
-    };
-
-    fetchSpring();
-  }, [selectedSpring]);
-
-  useEffect(() => {
-    const fetchSprings = async () => {
-      const springResponse = await listSprings();
-      if (!springResponse || springResponse.data?.length === 0) {
-        return;
-      }
-
-      console.log(springResponse.data);
-      setSprings(springResponse.data || []);
-    };
-
-    fetchSprings();
-  }, []);
-
-  useEffect(() => {
-    if (typeof window !== 'undefined' && 'geolocation' in navigator) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          setUserLocation({
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude,
-          });
-        },
-        (error) => {
-          console.error('Error getting user location:', error);
-          // Optionally handle error (e.g., show a toast notification)
-        }
-      );
-    }
-  }, []);
+  const { springsList, selectedSpring, getSpringById, isSpringsListLoading } = useDataContext();
 
   return (
     <main className="dashboard-container">
-      <DetailsPanel springs={springs} setSelectedSpring={setSelectedSpring} />
+      <DetailsPanel
+        springs={springsList}
+        onClickSpring={getSpringById}
+        isLoading={isSpringsListLoading}
+      />
       <section className="map-wrapper">
-        <Map springs={springs} selectedSpring={selectedSpring} userLocation={userLocation} />
+        <Map springs={springsList} selectedSpring={selectedSpring} />
       </section>
     </main>
   );
