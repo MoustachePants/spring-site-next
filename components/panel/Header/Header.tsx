a73cbfcf87591324c5853fe857cbfee10ba853be
royporat <roy.porat@bloxroute.com>
2025-11-29 16:11:13 +0200

    header

--- components/panel/Header/Header.tsx
+++ components/panel/Header/Header.tsx
@@ -3,14 +3,22 @@ import './Header.css';
 import Logo from '../../ui/Logo/Logo';
 import SearchInput from '../../SearchInput/SearchInput';
 import Filters from '../../panel/Filters/Filters';
+import Icons from '@/style/icons';
 
 const Header: React.FC = () => {
   return (
     <header className="header">
       <Logo size="L" />
-      <section className="header-filters">
-        <SearchInput />
-        <Filters />
+      <section className="header-filters-search">
+        <div className="header-filters-and-search">
+          <SearchInput />
+          <div className="filter-button">
+            <Icons.filter />
+            <span>מתקדם</span>
+          </div>
+        </div>
+
+        {/* <Filters /> */}
       </section>
     </header>
   );
