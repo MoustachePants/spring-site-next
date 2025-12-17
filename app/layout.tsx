b6765cb259a7293d5e7e15f7287552acca3ca2a2
MoustachePants <elad222@gmail.com>
2025-12-17 22:54:36 +0200

    claroty

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Suspense } from 'react';
 import { Toaster } from 'react-hot-toast';
 import { DataContextProvider } from '@/context/DataContext';
 import Loading from '@/components/loading/Loading/Loading';
+import Script from 'next/script';
 
 const notoSansHebrew = Noto_Sans_Hebrew({ subsets: ['hebrew'] });
 
@@ -122,6 +123,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="he" dir="rtl" suppressHydrationWarning={true}>
+      <Script type="text/javascript">
+          (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "un3xqv4mp4");
+      </Script>
       <body className={notoSansHebrew.className}>
         <Suspense fallback={<Loading />}>
           <DataContextProvider>
